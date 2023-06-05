29939fd510d53119ea7368cd5c5f63dff409961b
krisine <kristine.nyaga@student.moringaschool.com>
2023-06-05 21:59:58 +0300

    makes the game continue  after hitting the wall

--- snake.py
+++ snake.py
@@ -34,6 +34,14 @@ class Snake  :
         self.segments.append(newsquare)
 
         
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000,1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def extend(self):
         self.add_segment()
 
