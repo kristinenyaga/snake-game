4733eb17853755cfc379f417afdc997b8d235e79
krisine <kristine.nyaga@student.moringaschool.com>
2023-06-05 22:01:21 +0300

    change the game to reset the scoreboard

--- scoreboard.py
+++ scoreboard.py
@@ -1,10 +1,13 @@
 from turtle import Turtle
 ALIGNMENT = "center"
 FONT = "Courier"
+
+
 class ScoreBoard(Turtle):
-    def __init__(self):
+    def __init__(self,score):
         super().__init__()
         self.score = 0
+        self.high_score =score
         self.color("white")
         self.hideturtle()
         self.up()
@@ -14,9 +17,18 @@ class ScoreBoard(Turtle):
     def increase_score(self):
         self.clear()
         self.score+=1
-        self.write(f"Score : {self.score}",align=ALIGNMENT,font=(FONT, 12, 'bold'))
+        self.write(f"Score : {self.score} HighScore:{self.high_score  }",align=ALIGNMENT,font=(FONT, 12, 'bold'))
 
-    def game_over(self):
-        self.color("yellow")
-        self.goto(0,0)
-        self.write(f"GAME OVER",align=ALIGNMENT,font=(FONT, 20, 'bold'))
+        # def game_over(self):
+    #     self.color("yellow")
+    #     self.goto(0,0)
+    #     self.write(f"GAME OVER",align=ALIGNMENT,font=(FONT, 20, 'bold'))
+
+    def reset_scoreboard(self):
+        if self.score > int(self.high_score):
+            self.high_score = self.score
+            with open("data.txt",mode="w") as file:
+                file.write(str(self.high_score))
+                
+        self.score =0
+        self.increase_score()
